bd411de6f9a36b68500bc0126cabaef566f45c43
abkaur <159343131+abkaur@users.noreply.github.com>
2025-05-03 01:11:27 -0400

    Updated app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,19 +1,15 @@
 import os
-
 from flask import Flask
+
 admin_password = os.getenv("ADMIN_PASSWORD", "not-set")
-
 print("Admin Password (from env):", admin_password)
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Hello '+provider+'!'
+    return 'Hi, this is Abhijot. Welcome to my Flask app running in the cloud! 🚀'
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
